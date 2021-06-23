2eed76f30a5e6accc3b3f22f3239a8eadee790a4
AMAN SINGLA <amansingla222@gmail.com>
2021-06-23 16:17:33 +0530

    Implemented toggling features, footer, static-menus

--- src/Components/SideMenu/SideMenu.jsx
+++ src/Components/SideMenu/SideMenu.jsx
@@ -20,6 +20,70 @@ const SideMenu = () => {
 
             <div className='divider'>
             </div>
+
+            <div className='main-menu'>
+                <ul>
+                    <li>
+                        <a 
+                        href='abcd.com' className='menu-item'>
+                            <div className='menu-icon'>
+                            <i class="bi bi-speedometer2"></i>
+                            </div>
+                            <span>
+                            Dashboard
+                            </span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a 
+                        href='abcd.com' className='menu-item'>
+                            <div className='menu-icon'>
+                            <i class="bi bi-newspaper"></i>
+                            </div>
+                            <span>
+                                Content
+                            </span>
+                        </a>
+                        <ul className='sub-menu'>
+                            <li>
+                                <a>
+                                    Courses
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Videos
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a 
+                        href='abcd.com' className='menu-item'>
+                            <div className='menu-icon'>
+                            <i class="bi bi-vector-pen"></i>
+                            </div>
+                            <span>
+                                Design
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div className='side-menu-footer'>
+                <div className='avatar'>
+                    <img alt ='avatar'
+                     src = 'https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png'/>
+                </div>
+                <div className='user-info'>
+                    <h5>Aman Singla</h5>
+                    <p>amansingla222@gmail.com</p>
+                </div>
+            </div>
         </div>
     )
 }
